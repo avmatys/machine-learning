a6d10ea949db48b6ad2d2d1a6c78a5efbf49cd42
andrey_matys <matys.andrey@gmail.com>
2020-04-02 20:16:47 +0300

    Added solutions to tasks: computeCost.m,gradientDescent.m

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -4,19 +4,8 @@ function J = computeCost(X, y, theta)
 %   parameter for linear regression to fit the data points in X and y
 
 % Initialize some useful values
-m = length(y); % number of training examples
-
-% You need to return the following variables correctly 
-J = 0;
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta
-%               You should set J to the cost.
-
-
-
-
-
-% =========================================================================
-
+m = length(y); % number of training examples 
+h = X * theta; % hypothesis, vector m rows
+squared_error = (h - y).^ 2;
+J = (1/(2*m))*sum(squared_error);
 end
--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -5,27 +5,19 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 
 % Initialize some useful values
 m = length(y); % number of training examples
+n = length(theta); % number of features + 1
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
 
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCost) and gradient here.
-    %
-
-
-
-
-
-
-
-    % ============================================================
-
-    % Save the cost J in every iteration    
+    h = X * theta; % Calculate hypothesis (m x 1) 
+	error = h - y; % Calculate errors (m x 1)
+	steps = X' * error; % (n x m) * (m x 1) = (n x 1)
+	
+	% Calculate new thetas
+    theta = theta - alpha * (1/m) * steps; % (n x 1)
+	
+	% Calculate cost for new theta
     J_history(iter) = computeCost(X, y, theta);
 
 end
