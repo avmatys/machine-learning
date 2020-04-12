a706c86adc103fa895380b1dc993eff833059ccf
avmatys <andrey_matys@mail.ru>
2020-04-12 21:41:40 +0300

    Added solutions to tasks: lrCostFunction.m, oneVsAll.m, predictOneVsAll.m, predict.m

--- machine-learning-ex3/ex3/predictOneVsAll.m
+++ machine-learning-ex3/ex3/predictOneVsAll.m
@@ -17,26 +17,11 @@ p = zeros(size(X, 1), 1);
 % Add ones to the X data matrix
 X = [ones(m, 1) X];
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the following code to make predictions using
-%               your learned logistic regression parameters (one-vs-all).
-%               You should set p to a vector of predictions (from 1 to
-%               num_labels).
-%
-% Hint: This code can be done all vectorized using the max function.
-%       In particular, the max function can also return the index of the 
-%       max element, for more information see 'help max'. If your examples 
-%       are in rows, then, you can use max(A, [], 2) to obtain the max 
-%       for each row.
-%       
+% Calculate hypothesis
+h = X * all_theta';
 
-
-
-
-
-
-
-% =========================================================================
+% Select the highest one
+[maxProbabilities, p] = max(h, [], 2);
 
 
 end
